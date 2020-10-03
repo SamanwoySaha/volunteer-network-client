79a19fdffac6fcaf28a8ceeb5e273fc0f96934c4
SamanwoySaha <samanwoy.kuet@gmail.com>
2020-10-03 08:49:09 +0600

    initializing client site

---
+++ src/fakeData/fakeData.js
@@ -0,0 +1,88 @@
+const activities = [
+    {
+        name: 'Child Support',
+        picture: '',
+    },
+    {
+        name: 'Refuge Shelter',
+        picture: '',
+    },
+    {
+        name: 'Food Charity',
+        picture: '',
+    },
+    {
+        name: 'Host a clothing swap',
+        picture: '',
+    },
+    {
+        name: 'Host a river clean-up',
+        picture: '',
+    },
+    {
+        name: 'Clean water for Children',
+        picture: '',
+    },
+    {
+        name: 'Good Education',
+        picture: '',
+    },
+    {
+        name: 'Good Education',
+        picture: '',
+    },
+    {
+        name: 'New books for Children',
+        picture: '',
+    },
+    {
+        name: 'Host a study group',
+        picture: '',
+    },
+    {
+        name: 'Build bird houses for your neighbors',
+        picture: '',
+    },
+    {
+        name: 'Organize books at the library',
+        picture: '',
+    },
+    {
+        name: 'Give a seminar on driving safety',
+        picture: '',
+    },
+    {
+        name: 'Give free music lessons',
+        picture: '',
+    },
+    {
+        name: 'Teach people how to register to vote',
+        picture: '',
+    },
+    {
+        name: 'Clean up your local park',
+        picture: '',
+    },
+    {
+        name: 'Give IT help to local adults',
+        picture: '',
+    },
+    {
+        name: 'Foster a shelter animal',
+        picture: '',
+    },
+    {
+        name: 'Babysit during PTA meetings',
+        picture: '',
+    },
+    {
+        name: 'Collect stuffed animals',
+        picture: '',
+    },
+    {
+        name: 'Collect school supplies',
+        picture: '',
+    }
+];
+
+export default activities;
